7c0a2e264a7f8e0525bdff197bf74e8d6ff79922
naimuddin94 <developernaimuddin@gmail.com>
2023-11-27 14:05:43 +0600

    error handle route setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const globalErrorHandler = require("./utils/globalErrorHandler");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -10,6 +11,16 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("nexus news is running....");
 });
+
+// handling all route which is not found
+app.all("*", (req, res, next) => {
+  const error = new Error(`Can't find ${req.originalUrl} on the server`);
+  error.status = 404;
+  next(error);
+});
+
+// error handling middleware
+app.use(globalErrorHandler);
 
 const main = async () => {
   app.listen(port, () => {
