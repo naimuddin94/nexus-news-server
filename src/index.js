85afa45599ed1d44f6a54d672771cb38fe105741
naimuddin94 <developernaimuddin@gmail.com>
2023-11-29 09:36:10 +0600

    folder structure change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const globalErrorHandler = require("./utils/globalErrorHandler");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -8,19 +7,20 @@ const app = express();
 const userRoutes = require("./routes/userRoutes");
 const articleRoutes = require("./routes/articleRoutes");
 const authenticationRoute = require("./routes/authenticationRoute");
+const connectDB = require("./db/connectDB");
 
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://nexus-news-5bb9e.web.app",
+      "https://nexus-news-5bb9e.firebaseapp.com",
+    ],
     credentials: true,
   })
 );
 
-mongoose
-  .connect(process.env.DB_URI, { dbName: process.env.DB_NAME })
-  .then(() => console.log("DB connection established"))
-  .catch((error) => console.log(error));
 
 app.use("/users", userRoutes);
 app.use("/articles", articleRoutes);
@@ -40,6 +40,11 @@ app.all("*", (req, res, next) => {
 // error handling middleware
 app.use(globalErrorHandler);
 
-app.listen(port, () => {
-  console.log(`nexus news is running on port ${port}`);
-});
+const main = async () => {
+  await connectDB()
+  app.listen(port, () => {
+    console.log(`nexus news is running on port ${port}`);
+  });
+};
+
+main();
